709ddc99f4add5e1fa4a942bd81d831acb8be36b
Gustavo Gutierrez <gusguma@gmail.com>
2021-03-20 10:21:51 +0100

    LAB24-1 Modificaciones lab24_1.c

--- lab24_1.c
+++ lab24_1.c
@@ -7,34 +7,49 @@ Resultados:     Se mostrará el resultado en Días, Horas,
 Fecha:			Marzo 2021
 Autor:			Gustavo Gutierrez Martin
 Versión:		1.0.0
-*/ 
+*/
 
 #include<stdio.h>
 
-int obtieneDias(double horas);
-int obtieneHoras(double minutos);
-int obtieneMinutos(double segundos);
+const int DIA = 86400;
+const int HORA = 3600;
+const int MINUTO = 60;
+
+int entrada;
+int dias = 0;
+int horas = 0;
+int minutos = 0;
+int segundos = 0;
+int diasResto, horasResto;
+
+/**
+ * Metodo que obtiene los datos desglosados.
+ * 
+ * @return no devuelve nada
+ */
+int obtenerDatos();
 
 int main(){
-
-    int segundos;
-    int dias,horas,minutos;
-
-    
-
-
-
+    printf("\n---------------------------------------------------------------------\n");
+    printf("Programa para conversion de segundos en Dias,Horas,Minutos y Segundos\n");
+    printf("---------------------------------------------------------------------");
+    printf("\n\tIntroduzca el número de segundos:");
+    scanf("%d", &entrada);
+    obtenerDatos();
+    printf("\n\tLos segundos introducido son:\n");
+    printf("\t\t%i Días\n", dias);
+    printf("\t\t%i Horas\n", horas);
+    printf("\t\t%i Minutos\n", minutos);
+    printf("\t\t%i Segundos\n", segundos);
     return 0;
 }
 
-int obtieneDias(double horas) {
-    return 0;
-}
-
-int obtieneHoras(double minutos) {
-    return 0;
-}
-
-int obtieneMinutos(double segundos) {
+int obtenerDatos() {
+    dias = entrada / DIA;
+    diasResto = entrada % DIA;
+    horas = diasResto / HORA;
+    horasResto = diasResto % HORA;
+    minutos = horasResto / MINUTO;
+    segundos = horasResto % MINUTO;    
     return 0;
 }

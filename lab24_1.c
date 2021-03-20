060ab8717e1fef4285a902a522bac7b83932254c
Gustavo Gutierrez <gusguma@gmail.com>
2021-03-20 01:00:08 +0100

    LAB24-1 First Version C program

---
+++ lab24_1.c
@@ -0,0 +1,40 @@
+/*
+Programa:		lab24_1.c
+Objetivo:		Programa para la conversion de segundos en dias,
+                horas, minutos y segundos.
+Resultados:     Se mostrará el resultado en Días, Horas, 
+                Minutos y Segundos
+Fecha:			Marzo 2021
+Autor:			Gustavo Gutierrez Martin
+Versión:		1.0.0
+*/ 
+
+#include<stdio.h>
+
+int obtieneDias(double horas);
+int obtieneHoras(double minutos);
+int obtieneMinutos(double segundos);
+
+int main(){
+
+    int segundos;
+    int dias,horas,minutos;
+
+    
+
+
+
+    return 0;
+}
+
+int obtieneDias(double horas) {
+    return 0;
+}
+
+int obtieneHoras(double minutos) {
+    return 0;
+}
+
+int obtieneMinutos(double segundos) {
+    return 0;
+}

a17fc5b8f51a00ad78c418780eb0b8ffc4770857
Gustavo Gutierrez <gusguma@gmail.com>
2021-03-21 13:34:05 +0100

    LAB24-0 Cambios en varios ficheros

--- lab24_1.c
+++ lab24_1.c
@@ -22,34 +22,33 @@ int minutos = 0;
 int segundos = 0;
 int diasResto, horasResto;
 
-/**
- * Metodo que obtiene los datos desglosados.
- * 
- * @return no devuelve nada
- */
-int obtenerDatos();
-
+//programa principal
 int main(){
+
+    const int DIA = 86400;
+    const int HORA = 3600;
+    const int MINUTO = 60;
+
+    int dias = 0;
+    int horas = 0;
+    int minutos = 0;
+    int segundos = 0;
+    int entrada, diasResto, horasResto;
     printf("\n---------------------------------------------------------------------\n");
     printf("Programa para conversion de segundos en Dias,Horas,Minutos y Segundos\n");
     printf("---------------------------------------------------------------------");
     printf("\n\tIntroduzca el número de segundos:");
     scanf("%d", &entrada);
-    obtenerDatos();
-    printf("\n\tLos segundos introducido son:\n");
-    printf("\t\t%i Días\n", dias);
-    printf("\t\t%i Horas\n", horas);
-    printf("\t\t%i Minutos\n", minutos);
-    printf("\t\t%i Segundos\n", segundos);
-    return 0;
-}
-
-int obtenerDatos() {
     dias = entrada / DIA;
     diasResto = entrada % DIA;
     horas = diasResto / HORA;
     horasResto = diasResto % HORA;
     minutos = horasResto / MINUTO;
-    segundos = horasResto % MINUTO;    
+    segundos = horasResto % MINUTO; 
+    printf("\n\tLos segundos introducido son:\n");
+    printf("\t\t%i Días\n", dias);
+    printf("\t\t%i Horas\n", horas);
+    printf("\t\t%i Minutos\n", minutos);
+    printf("\t\t%i Segundos\n", segundos);
     return 0;
 }
--- lab24_2.c
+++ lab24_2.c
@@ -11,50 +11,55 @@ Versión:		1.0.0
 
 #include<stdio.h>
 
-const double EUR_USD = 1.1416;
-const double EUR_JPY = 124.34;
-const double EUR_GBP = 0.8860;
-const double EUR_CHF = 1.1297;
-const int DIVISION = 100;
-
-double entrada;
-int dolares = 0;
-int centavos = 0;
-int yenes = 0;
-int libras = 0;
-int peniques = 0;
-int francos = 0;
-int rappen = 0;
-
 /**
  * Obtiene los Dolares y los Centavos a partir de la entrada.
  * 
+ * @param entrada Euros introducidos por el usuario
+ * @param dolares Dolares obtenidos - por referencia
+ * @param centavos Centavos obtenidos - por referencia
  * @return no devuelve nada
  */
-int obtenerDolares();
+int obtenerDolares(double entrada, int *dolares, int *centavos);
 
 /**
  * Obtiene los Yenes a partir de la entrada.
  * 
+ * @param entrada Euros introducidos por el usuario
  * @return no devuelve nada
  */
-int obtenerYenes();
+int obtenerYenes(double entrada);
 
 /**
  * Obtiene las Libras y los Peniques a partir de la entrada.
  * 
+ * @param entrada Euros introducidos por el usuario
+ * @param libras Libras obtenidas - por referencia
+ * @param peniques Peniques obtenidos - por referencia
  * @return no devuelve nada
  */
-int obtenerLibras();
+int obtenerLibras(double entrada, int *libras, int *peniques);
 
 /**
- * Obtiene las Francos y los Rappen a partir de la entrada.
+ * Obtiene los Francos y los Rappen a partir de la entrada.
  * 
+ * @param entrada Euros introducidos por el usuario
+ * @param francos Francos obtenidos - por referencia
+ * @param peniques Rappen obtenidos - por referencia
  * @return no devuelve nada
  */
-int obtenerFrancos();
+int obtenerFrancos(double entrada, int *francos, int *rappen);
 
-int main(){
+int main(){    
+
+    double entrada;
+    int dolares = 0;
+    int centavos = 0;
+    int yenes = 0;
+    int libras = 0;
+    int peniques = 0;
+    int francos = 0;
+    int rappen = 0;
+
     printf("\n---------------------------------------------------------------------\n");
     printf("Programa para conversion de EUR en USD,YEN,GBP y CHF\n");
     printf("---------------------------------------------------------------------");
@@ -62,40 +67,46 @@ int main(){
     printf("\n\tformato siguiente: XXXXXXX.XX el numero de decimales será de 2");
     printf("\n\tIntroduzca los euros a convertir:");    
     scanf("%le", &entrada);
-    obtenerDolares();
-    obtenerYenes();
-    obtenerLibras();
-    obtenerFrancos();
+    obtenerDolares(entrada,&dolares,&centavos);
+    yenes = obtenerYenes(entrada);
+    obtenerLibras(entrada,&libras,&peniques);
+    obtenerFrancos(entrada,&francos,&rappen);
     printf("\n\t%.2f Euros equivalen a:\n\n",entrada);
-    printf("\t\t%d Dolares y %d Centavos.\n", dolares,centavos);
+    printf("\t\t%d Dolares y %d Centavos.\n",dolares,centavos);
     printf("\t\t%d Yenes.\n", yenes);
-    printf("\t\t%d Libras y %d Peniques.\n", libras,peniques);
-    printf("\t\t%d Francos y %d Rappens.\n\n", francos,rappen);
+    printf("\t\t%d Libras y %d Peniques.\n",libras,peniques);
+    printf("\t\t%d Francos y %d Rappens.\n\n",francos,rappen);
     return 0;
 }
 
-int obtenerDolares() {
+int obtenerDolares(double entrada, int *dolares, int *centavos) {
+    const int DIVISION = 100;
+    const double EUR_USD = 1.1416;
     double dolaresDouble = entrada * EUR_USD;
-    dolares = (int) dolaresDouble;
-    centavos = (dolaresDouble - dolares) * DIVISION;
+    *dolares = (int) dolaresDouble;
+    *centavos = (dolaresDouble - *dolares) * DIVISION;
     return 0;
 }
 
-int obtenerYenes() {
-    yenes = entrada * EUR_JPY;
-    return 0;
+int obtenerYenes(double entrada) {
+    const double EUR_JPY = 124.34;
+    return entrada * EUR_JPY;
 }
 
-int obtenerLibras() {
+int obtenerLibras(double entrada, int *libras, int *peniques) {
+    const int DIVISION = 100;
+    const double EUR_GBP = 0.8860;
     double librasDouble = entrada * EUR_GBP;
-    libras = (int) librasDouble;
-    peniques = (librasDouble - libras) * DIVISION;
+    *libras = (int) librasDouble;
+    *peniques = (librasDouble - *libras) * DIVISION;
     return 0;
 }
 
-int obtenerFrancos() {
+int obtenerFrancos(double entrada, int *francos, int *rappen) {
+    const int DIVISION = 100;
+    const double EUR_CHF = 1.1297;
     double francosDouble = entrada * EUR_CHF;
-    francos = (int) francosDouble;
-    rappen = (francosDouble - francos) * DIVISION;
+    *francos = (int) francosDouble;
+    *rappen = (francosDouble - *francos) * DIVISION;
     return 0;
 }
